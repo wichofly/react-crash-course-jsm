14f3a5208a78c2cc136d881b2fbf29e4f8f6d51a
Mauricio Erazo <antonierazo13@hotmail.com>
2023-02-17 20:13:15 +0100

    update: delete unnecesary files

--- src/App.js
+++ src/App.js
@@ -1,29 +1,5 @@
-import { useState, useEffect } from 'react';
-
-import './App.css';
-
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  useEffect(() => {
-    setCounter(1);
-  }, []);
-
-  return (
-    <div className="App">
-      <button
-        onClick={() =>
-          setCounter((prevCount) => (prevCount > 0 ? prevCount - 1 : 0))
-        }
-      >
-        -
-      </button>
-      <h1>{counter}</h1>
-      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>
-        +
-      </button>
-    </div>
-  );
+  return <h1>App</h1>;
 }
 
 export default App;
