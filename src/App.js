aaf9c65dbdd9b437dc283a5d4e9f994b642d8f6c
Mauricio Erazo <antonierazo13@hotmail.com>
2023-02-22 20:41:47 +0100

    feat: import searchIcon and Add comments

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import './App.css';
+import SearchIcon from './search.svg';
 
 import { useEffect } from 'react';
-/*
-  - We want to fetch the data from the api as soon as our component loads.
-  For that we use the hook "useEffect"
-  - An empty dependency array "[]" is used to call it at the start only. 
-*/
 
-// My API key: 49233842  49233842
+// My API key: 49233842
 
 const API_URL = 'http://www.omdbapi.com?apikey=49233842';
 
@@ -24,7 +20,31 @@ function App() {
     searchMovies('Coco');
   }, []);
 
-  return <h1>App</h1>;
+  /*
+  - We want to fetch the data from the api as soon as our component loads.
+    For that we use the hook "useEffect"
+    When you call useEffect, you’re telling React to run your “effect” function after flushing changes to the DOM.
+    Effects are declared inside the component so they have access to its props and state. 
+    By default, React runs the effects after every render — including the first render
+  - An empty dependency array "[]" is used to call it at the start only. 
+*/
+
+  return (
+    <div className="app">
+      <h1>MovieLand</h1>
+
+      <div className="search">
+        <input
+          placeholder="Find movies"
+          value={'Superman'}
+          onChange={() => {}}
+        />
+        <img src={SearchIcon} alt="search" />
+      </div>
+
+      <div></div>
+    </div>
+  );
 }
 
 export default App;
