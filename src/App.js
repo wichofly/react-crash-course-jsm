181055c4ff973ca9bcb82e85ea2279c77138b24b
Mauricio Erazo <antonierazo13@hotmail.com>
2023-02-22 21:13:27 +0100

    feat: create MovieCard component
    I passed the code from the movie card to a component cause are gonna be shown so many cards for a specific title on the search.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import MovieCard from './MovieCard';
 import SearchIcon from './search.svg';
 
 import { useEffect } from 'react';
@@ -52,27 +53,7 @@ function App() {
       </div>
 
       <div className="container">
-        <div className="movie">
-          <div>
-            <p>{movie1.Year}</p>
-          </div>
-
-          <div>
-            <img
-              src={
-                movie1.Poster !== 'N/A'
-                  ? movie1.Poster
-                  : 'https://via.placeholder.com/400'
-              }
-              alt={movie1.Title}
-            />
-          </div>
-
-          <div>
-            <span>{movie1.Type}</span>
-            <h3>{movie1.Title}</h3>
-          </div>
-        </div>
+        <MovieCard movie1={movie1} />
       </div>
     </div>
   );
